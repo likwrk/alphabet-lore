71d7062fb27b2bc9900f2c460484c7ddcd4d96fc
Pavel B <likwrk@gmail.com>
2023-01-07 12:41:46 +0300

    v1

---
+++ index.js
@@ -0,0 +1,23 @@
+function init() {
+  const container = document.querySelector('.container');
+  console.log('container', container);
+  container.addEventListener('click', (e) => {
+    const target = e.target;
+    const letter = target.getAttribute('data-letter');
+    if (!letter) {
+      return;
+    }
+    target.classList.add('zoom-in');
+    setTimeout(() => {
+      target.classList.remove('zoom-in');
+    }, 1000);
+    const a = new Audio('aac/' + letter + '.aac');
+    a.loop = false;
+    a.play();
+  });
+}
+
+(function() {
+  console.log('alt');
+  window.addEventListener('DOMContentLoaded', init);
+})();
